28adac5d028c2fd1aba8fec8ee93e3bda83ca1f4
prernabarodiya <151553142+prernabarodiya@users.noreply.github.com>
2023-11-22 19:04:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
-console.log("ajlskdfj");
-document.write("alsdkfj")
+function Print(){
+    var x = document.getElementById("uname").value;
+    var  y = document.getElementById("upass").value;
+    if(y == "Tinni") alert("Login Successfully");
+    else alert("Wrong Password");
+    alert("The name is " + x + "\n The password is " + y);
+
+
+}
