82d2a33d1483b004663c6ee9ca648adf54b80540
Prerna <151553142+prernabarodiya@users.noreply.github.com>
2023-11-24 19:37:12 +0530

    Create selectbox.js

---
+++ selectbox.js
@@ -0,0 +1,6 @@
+function selected(){
+    var x = document.getElementById("one");
+    if(x.options[x.selectedIndex].value == "Brendon Eich") alert("The Answer is Correct");
+    else alert("The anser is Wrong");
+    
+}
