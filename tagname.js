da3d66c1bd995a6166c3cb1d1ddbd886741ba4d1
Prerna <151553142+prernabarodiya@users.noreply.github.com>
2023-11-25 18:53:57 +0530

    Create tagname.js

---
+++ tagname.js
@@ -0,0 +1,8 @@
+function Fun2(){
+    var x = document.getElementsByTagName("div");
+
+    x[0].style.fontFamily = "Courier New";
+    x[1].style.fontWeight = "Bolder";
+    x[2].style.backgroundColor = "yellow";
+    x[3].style.color = "teal";
+}
